da3c15d63161a39b82907f4bd99038bc8276683e
Raihanarrasyid <raihanarrasyid1704@gmail.com>
2024-05-12 18:02:48 +0700

    feat : finish the projects

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import HeroAbout from "@/components/about/HeroAbout";
 import Todo from "@/components/about/Todo";
+import Travel from "@/components/home/Travel";
 import React, { Component } from "react";
 
 export default class About extends Component {
@@ -8,6 +9,7 @@ export default class About extends Component {
       <div>
         <HeroAbout />
         <Todo />
+        <Travel title="Get inspired for your journey" />
       </div>
     );
   }
